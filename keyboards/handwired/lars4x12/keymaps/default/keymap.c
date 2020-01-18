509a82a1706be33c53accfacab8a3a5b083f9c05
bluemoon <bluemoonhk15@gmail.com>
2020-01-18 20:31:54 +0800

    adjust keymap

--- keyboards/handwired/lars4x12/keymaps/default/keymap.c
+++ keyboards/handwired/lars4x12/keymaps/default/keymap.c
@@ -18,15 +18,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
    },
    {
 	{ KC_TILD  , KC_EXLM , KC_AT     , KC_HASH  , KC_DLR  , KC_PERC  , KC_CIRC  , KC_AMPR , KC_ASTR   , KC_LPRN   , KC_RPRN , KC_DEL   },
-	{ TR__     , NO__    , NO__      , KC_TILD  , KC_PIPE , KC_UNDS  , KC_PLUS  , KC_LCBR , KC_RCBR   , KC_PIPE   , TR__    , TR__     },
+	{ TR__     , NO__    , NO__      , NO__     , KC_TILD , KC_UNDS  , KC_PLUS  , KC_LCBR , KC_RCBR   , KC_PIPE   , TR__    , TR__     },
 	{ TR__     , KC_F1   , KC_F2     , KC_F3    , KC_F4   , KC_F5    , KC_F6    , NO__    , TR__      , TR__      , TR__    , TR__     },
-	{ TR__     , TR__    , TR__      , TR__     , TR__    , TR__     , TR__     , MO(3)   , TR__      , TR__      , TR__    , TR__     }
+	{ TR__     , TR__    , TR__      , TR__     , TO(0)   , TR__     , TR__     , MO(3)   , TR__      , TR__      , TR__    , TR__     }
    },
    {
 	{ KC_GRV   , KC_1    , KC_2      , KC_3     , KC_4    , KC_5     , KC_6     , KC_7    , KC_8      , KC_9      , KC_0    , KC_DEL   },
-	{ TR__     , NO__    , NO__      , KC_NUHS  , KC_NUBS , KC_MINS  , KC_EQL   , KC_LBRC , KC_RBRC   , KC_BSLS   , TR__    , TR__     },
+	{ TR__     , NO__    , NO__      , NO__     , KC_GRV  , KC_MINS  , KC_EQL   , KC_LBRC , KC_RBRC   , KC_BSLS   , TR__    , TR__     },
 	{ TR__     , KC_F7   , KC_F8     , KC_F9    , KC_F10  , KC_F11   , KC_F11   , NO__    , TR__      , TR__      , TR__    , TR__     },
-	{ TR__     , TR__    , TR__      , TR__     , MO(3)   , TR__     , TR__     , TR__    , TR__      , TR__      , TR__    , TR__     }
+	{ TR__     , TR__    , TR__      , TR__     , MO(3)   , TR__     , TR__     , TO(0)   , TR__      , TR__      , TR__    , TR__     }
    },
    {
 	{ RESET    , NO__    , NO__      , NO__     , NO__    , NO__     , NO__     , NO__    ,NO__       , NO__      , NO__    , NO__     },
