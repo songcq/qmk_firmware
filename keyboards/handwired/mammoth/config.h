a41d7a7f4d32d8d4f6e9a968ae87314bd04f2370
songcq <songcq@gmail.com>
2020-03-25 21:04:17 +0000

    add

--- keyboards/handwired/mammoth/config.h
+++ keyboards/handwired/mammoth/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS {B2, B6, B4, B5 }
-#define MATRIX_COL_PINS {E6, D7, C6, D4, D0,  F5, F6, F7, B1, B3 }
+#define MATRIX_COL_PINS {B3, B1, F7, F6, F5, D0, D4, C6, D7, E6}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
--- keyboards/handwired/mammoth/keymaps/default/keymap.c
+++ keyboards/handwired/mammoth/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	KC_Q     , KC_W      , KC_E     , KC_R    , KC_T     , KC_Y     , KC_U    , KC_I      , KC_O      , KC_P     ,
 	KC_A     , KC_S      , KC_D     , KC_F    , KC_G     , KC_H     , KC_J    , KC_K      , KC_L      , KC_UP    ,
 	S_(Z)    , KC_X      , KC_C     , KC_V    , KC_B     , KC_N     , KC_M    , KC_LEFT   , KC_RGHT   , S_(DOWN) ,
-    W_(BSPC) , TT(1)     , C_(SPC)  , A_(ESC) ,                       A_(TAB) , C_(ENT)   , TT(2)     , W_(DEL)
+    W_(DEL)  , TT(1)     , C_(BSPC) , A_(ESC) ,                       A_(TAB) , C_(ENT)   , TT(2)     , W_(SPC)
 	),
    LAYOUT(
 	KC_EXLM  , KC_AT     , KC_HASH  , KC_DLR  , KC_PERC  , KC_CIRC  , KC_AMPR , KC_ASTR   , KC_LPRN   , KC_RPRN  ,
