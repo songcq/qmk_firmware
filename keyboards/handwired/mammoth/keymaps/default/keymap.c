90261c9490bde12e57598591cc56fe6057a9f815
songcq <songcq@gmail.com>
2020-03-26 20:09:16 +0000

    adjust keymap

--- keyboards/handwired/mammoth/keymaps/default/keymap.c
+++ keyboards/handwired/mammoth/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	KC_Q     , KC_W      , KC_E     , KC_R    , KC_T     , KC_Y     , KC_U    , KC_I      , KC_O      , KC_P     ,
 	KC_A     , KC_S      , KC_D     , KC_F    , KC_G     , KC_H     , KC_J    , KC_K      , KC_L      , KC_UP    ,
 	S_(Z)    , KC_X      , KC_C     , KC_V    , KC_B     , KC_N     , KC_M    , KC_LEFT   , KC_RGHT   , S_(DOWN) ,
-    W_(DEL)  , TT(1)     , C_(BSPC) , A_(ESC) ,                       A_(TAB) , C_(ENT)   , TT(2)     , W_(SPC)
+    W_(BSPC) , TT(1)     , C_(DEL)  , A_(ESC) ,                       A_(TAB) , C_(ENT)   , TT(2)     , W_(SPC)
 	),
    LAYOUT(
 	KC_EXLM  , KC_AT     , KC_HASH  , KC_DLR  , KC_PERC  , KC_CIRC  , KC_AMPR , KC_ASTR   , KC_LPRN   , KC_RPRN  ,
@@ -34,23 +34,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	NO__     , NO__      , NO__     , NO__    , NO__     , NO__     , NO__    , NO__      , NO__      , RESET    ,
     NO__     , NO__      , NO__     , NO__    ,                       NO__    , NO__      , NO__      , NO__
 	),
-   
-  /*  { */
-  /* 	{ KC_TILD  , KC_EXLM , KC_AT     , KC_HASH  , KC_DLR  , KC_PERC  , KC_CIRC  , KC_AMPR , KC_ASTR   , KC_LPRN   , KC_RPRN , KC_DEL   }, */
-  /* 	{ TR__     , NO__    , NO__      , NO__     , KC_TILD , KC_UNDS  , KC_PLUS  , KC_LCBR , KC_RCBR   , KC_PIPE   , TR__    , TR__     }, */
-  /* 	{ TR__     , KC_F1   , KC_F2     , KC_F3    , KC_F4   , KC_F5    , KC_F6    , NO__    , TR__      , TR__      , TR__    , TR__     }, */
-  /* 	{ TR__     , TR__    , TR__      , TR__     , TO(0)   , TR__     , TR__     , MO(3)   , TR__      , TR__      , TR__    , TR__     } */
-  /*  }, */
-  /*  { */
-  /* 	{ KC_GRV   , KC_1    , KC_2      , KC_3     , KC_4    , KC_5     , KC_6     , KC_7    , KC_8      , KC_9      , KC_0    , KC_DEL   }, */
-  /* 	{ TR__     , NO__    , NO__      , NO__     , KC_GRV  , KC_MINS  , KC_EQL   , KC_LBRC , KC_RBRC   , KC_BSLS   , TR__    , TR__     }, */
-  /* 	{ TR__     , KC_F7   , KC_F8     , KC_F9    , KC_F10  , KC_F11   , KC_F11   , NO__    , TR__      , TR__      , TR__    , TR__     }, */
-  /* 	{ TR__     , TR__    , TR__      , TR__     , MO(3)   , TR__     , TR__     , TO(0)   , TR__      , TR__      , TR__    , TR__     } */
-  /*  }, */
-  /*  { */
-  /* 	{ RESET    , NO__    , NO__      , NO__     , NO__    , NO__     , NO__     , NO__    ,NO__       , NO__      , NO__    , NO__     }, */
-  /* 	{ KC_VOLD  , KC_VOLU , KC_MUTE   , NO__     , NO__    , NO__     , NO__     , NO__    ,NO__       , NO__      , NO__    , NO__     }, */
-  /* 	{ KC_BRID  , KC_BRIU , NO__      , NO__     , NO__    , NO__     , NO__     , NO__    ,NO__       , NO__      , NO__    , NO__     }, */
-  /* 	{ NO__     , NO__    , NO__      , NO__     , NO__    , NO__     , NO__     , NO__    ,NO__       , NO__      , NO__    , NO__     }, */
-  /*  }, */
   };
